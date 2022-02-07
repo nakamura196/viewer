181252a31b843cbc0f9acce00972440f8f4c4b15
nakamura196 <nakamura196@users.noreply.github.com>
2022-02-07 14:23:11 +0000

    deploy: b1f257f2728144a87c42536dd9168f469c14ef94

--- _nuxt/444ee6b.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{291:function(e){e.exports=JSON.parse('{"IIIF Viewers":"IIIF Viewers","ldas":"ldas","add":"Add","open":"Open","manifest":"Manifest","canvas":"Canvas"}')}}]);
--- _nuxt/7b1cd72.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{292:function(e){e.exports=JSON.parse('{"IIIF Viewers":"IIIF Viewers","ldas":"ldas","add":"追加","open":"開く","manifest":"マニフェスト","canvas":"カンバス"}')}}]);
